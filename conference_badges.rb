665ee00a503963eb9c904fdcdebbd3c16b12e5b3
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-09-18 14:04:28 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  names.collect {|name| badge_maker(name)}
+end
+
+def assign_rooms(names)
+  speakers.collect.with_index {|name, i| "Hello, #{name}! You'll be assigned to room #{i+1}!"}
+end
+
+def printer
+  batch_badge_creator.each {|batch| puts batch}
+  assign_rooms.each {|room| puts room}
+end
